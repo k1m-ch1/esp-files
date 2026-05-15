7c64fae1a4d32e6250b2a929a73d4264f6eebdd0
k1mch1 <kimchourluy@gmail.com>
2026-05-16 03:02:56 +0700

    finished modified python server

--- d1_mini_clone/postWeather/postWeather.ino
+++ d1_mini_clone/postWeather/postWeather.ino
@@ -2,52 +2,52 @@
 #include "env.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <Ticker.h>
 
 #define URL "http://192.168.1.145:10000"
 
 #define DHT_PIN 2
 
-#define TIMEOUT 10
+#define WIFI_TIMEOUT_MS 10000
+#define HTTP_TIMEOUT_MS 5000
 #define SLEEP_DURATION 5e6
-
-Ticker timeOutSleepTimer;
-
-void timeOutSleep(){
-  ESP.deepSleep(SLEEP_DURATION);
-}
 
 void setup(){
   Serial.begin(115200);
   pinMode(DHT_PIN, INPUT);
-  timeOutSleepTimer.attach(TIMEOUT, timeOutSleep);
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  int wifiStart = millis();
   Serial.print("Trying to connect to wifi");
   while (WiFi.status() != WL_CONNECTED) {
+    if(millis() - wifiStart > WIFI_TIMEOUT_MS){
+      Serial.printf("[WiFi] time out of %d milliseconds has reached. Forcefully going to sleep.\n", WIFI_TIMEOUT_MS);
+      ESP.deepSleep(SLEEP_DURATION);
+    }
     Serial.print('.');
     delay(500);
   }
   Serial.println();
   Serial.printf("[WiFi] got ip: %s\n", WiFi.localIP().toString().c_str());
-  HTTPClient http;
-  WiFiClient client;
-  if(http.begin(client, URL)){
-    http.addHeader("Content-Type", "application/json");
-    float temperature;
-    float humidity;
-    readHumidityAndTemperature(DHT_PIN, &temperature, &humidity);
-    String httpRequestData = String("{\"key\": \"") + KEY +
-      String("\",\"temperature\":") + String(temperature) +
-      String(",\"humidity\":") + String(humidity) +
-      String("}");
-    if(http.POST(httpRequestData) > 0){
-      String data = http.getString();
-      Serial.println(data);
+  float temperature;
+  float humidity;
+  if(!readHumidityAndTemperature(DHT_PIN, &temperature, &humidity)){
+    HTTPClient http;
+    WiFiClient client;
+    http.setTimeout(HTTP_TIMEOUT_MS);
+    if(http.begin(client, URL)){
+      http.addHeader("Content-Type", "application/json");
+      String httpRequestData = String("{\"key\": \"") + KEY +
+        String("\",\"temperature\":") + String(temperature) +
+        String(",\"humidity\":") + String(humidity) +
+        String("}");
+      if(http.POST(httpRequestData) > 0){
+        String data = http.getString();
+        Serial.println(data);
+      }
     }
+    http.end();
+    client.stop();
   }
-  http.end();
-  client.stop();
   ESP.deepSleep(SLEEP_DURATION);
 }
 
--- python_http_server/main.py
+++ python_http_server/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from datetime import datetime
+import csv
+import json
+
+WEATHER_DATA = "./weather_data.csv"
 
 app = FastAPI()
 
@@ -14,10 +18,8 @@ class Weather(BaseModel):
 
 @app.post('/')
 async def handle_post_request(weather: Weather):
-    print(f"your key: {weather.key.strip()}")
-    print(f"expected key: {KEY.strip()}")
     if weather.key.strip() == KEY.strip():
-        with open("./weather_data.csv", 'a') as file:
+        with open(WEATHER_DATA, 'a') as file:
             date = datetime.now().isoformat()
             humidity = round(weather.humidity, 2)
             temperature = round(weather.temperature, 2)
@@ -35,7 +37,6 @@ async def handle_post_request(weather: Weather):
 
 @app.get('/')
 async def handle_get_request():
-    return {
-        "message": "what's going on!"
-    }
-
+    with open(WEATHER_DATA, newline="") as file:
+        data = list(csv.DictReader(file))
+    return data
