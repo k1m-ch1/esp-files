347e759e08988e7015eae69b096f2af7153386b4
k1mch1 <kimchourluy@gmail.com>
2026-06-01 18:07:50 +0700

    did baremetal arduino

---
+++ nano_baremetal/blink/main.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DELAY_TIME 1000
+
+int main() {
+  DDRB |= (1 << PB5);
+  while (1) {
+    PORTB |= (1 << PB5);
+    _delay_ms(DELAY_TIME);
+    PORTB &= ~(1 << PB5);
+    _delay_ms(DELAY_TIME);
+  }
+}
